f1d4438efe781419abda89e271a139e2b6c59fe6
knewRock <who-are-you-._@hotmail.com>
2015-11-13 18:54:50 +0700

    connecting with database

--- easyrules-archetype/src/main/java/org/amhungry/Launcher.java
+++ easyrules-archetype/src/main/java/org/amhungry/Launcher.java
@@ -2,6 +2,10 @@ package org.amhungry;
 
 import org.easyrules.api.RulesEngine;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -125,5 +129,40 @@ public class Launcher {
         sortArray();
    
         printAllRestaurant();
+        
+        
+        Connection c = null;
+        Statement stmt = null;
+        try {
+          Class.forName("org.sqlite.JDBC");
+          c = DriverManager.getConnection("jdbc:sqlite:db.sqlite3");
+          System.out.println("path");
+          c.setAutoCommit(false);
+          System.out.println("Opened database successfully");
+
+          stmt = c.createStatement();
+          ResultSet rs = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type='table';");
+
+          while ( rs.next() ) {
+             String  name = rs.getString("name");
+             System.out.println( "NAME = " + name );
+             System.out.println();
+          }    
+
+          stmt = c.createStatement();
+          ResultSet rs1 = stmt.executeQuery("SELECT * FROM restaurant_restaurant;");
+          while ( rs1.next() ) {
+            int id = rs1.getInt("id");
+             String  name = rs1.getString("name");
+             System.out.println( "ID = " + id );
+             System.out.println( "NAME = " + name );
+             System.out.println();
+          }    c.close();
+
+        } catch ( Exception e ) {
+          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+          System.exit(0);
+        }
+        System.out.println("Operation done successfully");
     }
 }
